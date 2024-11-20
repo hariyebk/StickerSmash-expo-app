d259ae091b9e3c003edb7b4d1657617ddbb1a542
hariyebk <harunbekri6@gmail.com>
2024-11-20 10:43:25 +0300

    adding platform specific behavior

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,6 +12,8 @@ import { type ImageSource } from 'expo-image';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as MediaLibrary from "expo-media-library"
 import { captureRef } from 'react-native-view-shot';
+import { Platform } from 'react-native';
+import domtoimage from 'dom-to-image';
 
 const PlaceholderImage = require('@/assets/images/background-image.png')
 
@@ -56,19 +58,38 @@ export default function Home(){
   };
 
   const onSaveImageAsync = async () => {
-    try{
-      // we are taking a screenshot here
-      const localURI = await captureRef(imageRef, {
-        height: 440,
-        quality: 1
-      })
-      await MediaLibrary.saveToLibraryAsync(localURI);
-      if(localURI){
-        alert('Saved!')
+    if(Platform.OS !== "web"){
+      try{
+        // we are taking a screenshot here
+        const localURI = await captureRef(imageRef, {
+          height: 440,
+          quality: 1
+        })
+        await MediaLibrary.saveToLibraryAsync(localURI);
+        if(localURI){
+          alert('Saved!')
+        }
+      }
+      catch(e){
+        console.log(e)
       }
     }
-    catch(e){
-      console.log(e)
+    else{
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      }
+      catch (e) {
+        console.log(e);
+      }
     }
   };
 
